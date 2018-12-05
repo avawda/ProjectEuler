8b176e33e71b9a7cc74439b5ce4efe353b63140e
Ahmed <ahmed.vawda@gmail.com>
2018-12-05 15:02:02 +0200

    Trying to get GIT working behind proxy

--- Problem11.py
+++ Problem11.py
@@ -4,7 +4,7 @@ import Eutils as et
 
 # PROBLEM STATEMENT
 # https://projecteuler.net/problem=11
-# In the 20×20 grid below, four numbers along a diagonal line have been marked in red. (have marked in brackets below)
+# In the 20×20 grid below, four numbers along a diagonal line have been marked in red. (I have marked the "red" numbers in brackets below)
 
 # 08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
 # 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
